d0a9a0973a0294e745b7a7f3fb20a27932529e60
Khanhan2k2 <100439816+Khanhan2k2@users.noreply.github.com>
2022-02-27 17:04:33 +0700

    Add files via upload

---
+++ Test OOP/Bill.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Test_OOP
+{
+   public class Bill
+    {
+        private string _idb="";
+        private string _date="";
+        private Customer _quest=new Customer();
+        private List<Detai_lbill> _ldBill=new List<Detai_lbill>();
+        private int _nodb=0;
+        private double _total=0;
+        public void InPut()
+        {
+            do
+            {
+                Console.Write("Mã hóa đơn(3-10 kí tự gồm số hoặc chữ,VD:HD003): ");
+                _idb = Console.ReadLine();
+            } while (!IsID(_idb));
+            
+            do
+            {
+                Console.Write("Ngày lập hóa đơn: ");
+                _date = Console.ReadLine();
+            }
+            while (_date == "" || !Is_date(_date)) ;
+            
+            Console.WriteLine("Thông tin khách hàng: ");
+            _quest.Input();
+            Console.WriteLine("Nhập danh sách các chi tiết hóa đơn: ");
+            do
+            {
+                
+               
+                Console.Write("\t Số lượng chi tiết trong dah sách các chi tiết hóa đơn: ");
+                try
+                {
+                    _nodb = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("\tvui lòng nhập số nguyên dương");
+                    continue;
+                }
+            } while (_nodb<=0);
+            for (int i = 0; i < _nodb; i++)
+            {
+                Detai_lbill temp = new Detai_lbill();
+                Console.WriteLine("\tNhập chi tiết hóa đơn thứ " + (i + 1));
+                temp.InPut();
+                temp.Price();
+                _ldBill.Add(temp);
+                _total += temp._DBcost;
+            }
+        }
+        public void OutPut()
+        {
+            Console.WriteLine("Hóa đơn: " + _idb + " " + _date+" "+_total);
+            _quest.Output();
+            for(int i=0;i<_ldBill.Count;i++)
+            {
+                _ldBill[i].Output();
+            }    
+            
+        }
+        protected bool Is_date(string temp_date)
+        {
+            DateTime from_dateValue;
+            var formats = new[] { "dd/MM/yyyy", "yyyy-MM-dd","d/M/yyyy","dd/M/yyyy","d/MM/yyyy" };
+            if (DateTime.TryParseExact(temp_date, formats, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out from_dateValue))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        protected bool IsID(string ID)
+        {
+            if (ID.Length < 3 || ID.Length > 10)
+            {
+                Console.WriteLine("\t\t\tMã thường gồm 3-10 kí tự gồm chữ hoặc số");
+                return false;
+            }
+            else
+            {
+                if (ID.Contains(" "))
+                {
+                    Console.WriteLine("\t\t\tMã thường gồm 3-10 kí tự gồm chữ hoặc số");
+                    return false;
+                }
+                if (System.Text.RegularExpressions.Regex.Match(ID, @"[a-zA-Z_0-9]").Success) return true;
+                else
+                {
+                    Console.WriteLine("\t\t\tMã thường gồm 3-10 kí tự gồm chữ hoặc số");
+                    return false;
+                }
+            }
+        }
+        public void OutToText()
+        {
+            StreamWriter sw = File.AppendText(Environment.CurrentDirectory + @"\danh_sach_hoa_don.txt");
+            sw.WriteLine("Mã hóa đơn: " + _idb);
+            sw.WriteLine("Ngày lập hóa đơn(dd/mm/yyyy): " + _date);
+            sw.WriteLine("Thông tin khách hàng: ");
+            sw.Close();
+            _quest.OutToText();
+            sw.Close();
+            for (int i = 0; i < _nodb; i++)
+            {
+                sw = File.AppendText(Environment.CurrentDirectory + @"\danh_sach_hoa_don.txt");
+                sw.WriteLine("\tNhập chi tiết hóa đơn thứ " + (i + 1));
+                sw.Close();
+                _ldBill[i].OutToText();
+            }
+        }
+
+    }
+}
---
+++ Test OOP/Product.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Test_OOP
+{
+    public class Product
+    {
+        protected string IDP="";
+        protected string NameP="";
+        protected string Where="";
+        protected double Cost=0;
+        protected int TypeP=0;
+        protected int Amout=0;
+        public int _TypeP
+            {
+            get { return TypeP; }
+            set { TypeP = value; }
+        }
+        public int _Amout
+        {
+            get { return Amout; }
+            set { Amout = value; }
+        }
+        public virtual  void InPut()
+        {
+            
+            while (_TypeP<=0 || _TypeP>2)
+            {
+                Console.Write("\t\tChọn loại thiết bị điện (1-máy quạt , 2- máy lạnh):");
+                try
+                {
+                    _TypeP = Int32.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Nhập 1 hoặc 2");
+                }
+            }
+            
+        }
+        public virtual void OutPut()
+        {
+
+        }
+        public virtual double Price()
+        {
+            return 0;
+        }
+        public virtual void OutToText()
+        {
+            
+        }
+
+    }
+}
